532a1443600c409e827ea860586110c348d2d62a
Tianai <tashang6002@gmail.com>
2025-02-25 13:12:15 -0800

    color

--- coursecadmey/lib/profilepage.dart
+++ coursecadmey/lib/profilepage.dart
@@ -4,94 +4,112 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'), // 页面标题
-        backgroundColor: const Color.fromRGBO(255, 210, 2, 1), //
-      ),
-      body: SingleChildScrollView(
-        // 使用 SingleChildScrollView 使页面可以滚动
-        child: Column(
-          children: [
-            // 用户信息部分
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 50, // 头像大小
-                    backgroundImage:
-                        NetworkImage('https://via.placeholder.com/150'), // 用户头像
-                  ),
-                  SizedBox(width: 16), // 添加间距
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Username', // 用户名
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        '1000 Subscribers', // 订阅人数
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      ElevatedButton(
-                        onPressed: () {
-                          // 处理订阅逻辑
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromRGBO(255, 210, 2, 1), // 按钮颜色
-                        ),
-                        child: Text('Subscribe'),
-                      ),
+      body: CustomScrollView(
+        slivers: [
+          // SliverAppBar 部分
+          SliverAppBar(
+            expandedHeight: 100.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(255, 242, 179, 1),
+                      Color.fromRGBO(255, 255, 255, 1)
                     ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                   ),
-                ],
+                ),
               ),
             ),
-            // 视频列表部分
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Videos',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+          ),
+
+          SliverList(
+            delegate: SliverChildListDelegate([
+              // 用户信息部分
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 50, // 头像大小
+                      backgroundImage: NetworkImage(
+                          'https://via.placeholder.com/150'), // 用户头像
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  // 视频项示例
-                  VideoItem(
-                    thumbnailUrl: 'https://via.placeholder.com/150',
-                    title: 'Video Title 1',
-                    views: '1,000 views',
-                  ),
-                  VideoItem(
-                    thumbnailUrl: 'https://via.placeholder.com/150',
-                    title: 'Video Title 2',
-                    views: '500 views',
-                  ),
-                  VideoItem(
-                    thumbnailUrl: 'https://via.placeholder.com/150',
-                    title: 'Video Title 3',
-                    views: '200 views',
-                  ),
-                ],
+                    SizedBox(width: 16), // 添加间距
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Username', // 用户名
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          '1000 Subscribers', // 订阅人数
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        ElevatedButton(
+                          onPressed: () {
+                            // 处理订阅逻辑
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromRGBO(255, 210, 2, 1), // 按钮颜色
+                          ),
+                          child: Text('Subscribe'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
+              // 视频列表部分
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Videos',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    // 视频项示例
+                    VideoItem(
+                      thumbnailUrl: 'https://via.placeholder.com/150',
+                      title: 'Video Title 1',
+                      views: '1,000 views',
+                    ),
+                    VideoItem(
+                      thumbnailUrl: 'https://via.placeholder.com/150',
+                      title: 'Video Title 2',
+                      views: '500 views',
+                    ),
+                    VideoItem(
+                      thumbnailUrl: 'https://via.placeholder.com/150',
+                      title: 'Video Title 3',
+                      views: '200 views',
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
@@ -112,7 +130,8 @@ class VideoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0), // 上下间距
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      elevation: 2,
       child: Row(
         children: [
           Image.network(
@@ -120,6 +139,14 @@ class VideoItem extends StatelessWidget {
             width: 120,
             height: 80,
             fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Container(
+                width: 120,
+                height: 80,
+                color: Colors.grey,
+                child: Icon(Icons.error),
+              );
+            },
           ),
           SizedBox(width: 8),
           Expanded(
